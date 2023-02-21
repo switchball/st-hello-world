1a4de6c7de5799162b50156aec9117edcc38e5dd
Art9 <xyc5000@hotmail.com>
2023-02-21 13:22:06 +0800

    Update hello.py
    add secret test

--- hello.py
+++ hello.py
@@ -4,3 +4,18 @@ st.write("""
 # Hello World!
 This is the first app created via streamlit.
 """)
+
+# Everything is accessible via the st.secrets dict:
+
+st.write("Day Limit:", st.secrets["day_limit"])
+st.write("My cool secrets:", st.secrets["api_key"]["secret_key"])
+st.write("My cool secrets:", st.secrets["api_key"]["extra_keys"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["DAY_LIMIT"] == st.secrets["DAY_LIMIT"],
+)
